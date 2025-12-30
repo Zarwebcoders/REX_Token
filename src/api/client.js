168916a3968a93e9a0bcf3df212359db5cc1299d
zarwebcoders <zarwebcoders@gmail.com>
2025-12-30 20:46:17 +0530

    All Errors Fixed

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: import.meta.env.PROD ? 'https://rex-token-backend-beta.vercel.app/' : '/api',
+    baseURL: import.meta.env.PROD ? 'https://rex-token-backend-beta.vercel.app/api' : '/api',
     headers: {
         'Content-Type': 'application/json',
     },
